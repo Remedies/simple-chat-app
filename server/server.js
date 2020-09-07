ee35e1a9cf3571c4920428f36e90ecdc220ccf88
chris <chrisreycap@gmail.com>
2020-09-07 16:16:29 -0500

    added first view

--- server/server.js
+++ server/server.js
@@ -7,14 +7,17 @@ const app = express();
 
 //constants
 const PORT = process.env.PORT || 5000;
-const staticPath = path.join(__dirname, './client/assets/');
+const staticPath = path.join(__dirname, './../client/assets/');
+const viewsPath = path.join(__dirname, './../client/views');
 
 //app configurations
 app.use(express.static(staticPath));
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
 
 //routes
 app.get('*', (req, res) => {
-    res.status(200).send('Hello!');
+    res.render('index', { myVariable: 'test' });
 });
 
 const server = app.listen(PORT, () => {
