8809face7937a175ce5ad7419e9b14663b380c79
chris <chrisreycap@gmail.com>
2020-09-07 20:18:26 -0500

    simple styling foundation

--- client/assets/js/sockets.js
+++ client/assets/js/sockets.js
@@ -4,6 +4,7 @@ const message = document.getElementById('message-box');
 const handle = document.getElementById('handle-box');
 const button = document.getElementById('send-button');
 const chat = document.getElementById('chat-box');
+const status = document.getElementById('chat-status');
 
 socket.on('connect', () => {
     console.log('connected!');
@@ -15,6 +16,7 @@ button.addEventListener('click', (e) => {
         message: message.value,
         handle: handle.value,
     });
+    message.value = '';
 });
 
 message.addEventListener('keypress', (e) => {
@@ -24,9 +26,10 @@ message.addEventListener('keypress', (e) => {
 });
 
 socket.on('chat', (data) => {
-    chat.innerHTML += `<p><strong>${data.handle}:<strong> ${data.message}</p>`;
+    chat.innerHTML += `<div class="chat-message"><span class='chat-handle'>${data.handle}:<span><span class='chat-message'>${data.message}</span><br></div>`;
+    status.innerHTML = ' ';
 });
 
 socket.on('typing', (data) => {
-    chat.innerHTML = `<p>${data.handle} is typing...</p>`;
+    status.innerHTML = `${data.handle} is typing...<br>`;
 });
